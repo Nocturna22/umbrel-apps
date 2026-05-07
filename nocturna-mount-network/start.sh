3932839df30050788afa017731f98b5ba029e0a6
Nocturna22 <46491831+Nocturna22@users.noreply.github.com>
2026-05-07 02:39:38 +0200

    Add files via upload

---
+++ nocturna-mount-network/start.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#echo "Installing CIFS tools..."
+#apk add --no-cache cifs-utils
+
+USERNAME="Username"
+PASSWORD="Password"
+SERVER="192.168.111.1"
+SHARE="Fritzbox-Fynn-NAS"
+
+MOUNT_BASE="/mnt/network"
+MOUNT_PATH="$MOUNT_BASE/$SERVER/$SHARE"
+
+echo "Creating mount directory: $MOUNT_PATH"
+mkdir -p "$MOUNT_PATH"
+
+echo "Mounting CIFS share..."
+
+mount -t cifs \
+  -o rw,vers=3.0,noserverino,username=$USERNAME,password="$PASSWORD" \
+  "//$SERVER/$SHARE/" \
+  "$MOUNT_PATH"
+
+if [ $? -eq 0 ]; then
+  echo "Mount successful"
+else
+  echo "Mount failed"
+fi
+
+# Container alive halten
+tail -f /dev/null
